12c0c1588239b0c296001a4eb408d03f75015671
unknown <cn528491@gmail.com>
2022-06-03 06:08:03 +0400

    add email

--- backend/src/main/java/ge/propertygeorgia/catalogue/subscriber/SubscriberService.java
+++ backend/src/main/java/ge/propertygeorgia/catalogue/subscriber/SubscriberService.java
@@ -1,8 +1,23 @@
 package ge.propertygeorgia.catalogue.subscriber;
 
+import org.apache.commons.csv.CSVFormat;
+import org.apache.commons.csv.CSVPrinter;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.supercsv.io.CsvBeanWriter;
+import org.supercsv.io.ICsvBeanWriter;
+import org.supercsv.prefs.CsvPreference;
 
+import javax.servlet.http.HttpServletResponse;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.nio.file.FileSystems;
+import java.nio.file.Path;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 import java.util.List;
 
 @Service
@@ -21,6 +36,35 @@ public class SubscriberService {
 
     public void postSubscriber(Subscriber subscriber) {
         subscriberRepository.save(subscriber);
+    }
+
+    public Path exportToCSV() throws IOException {
+        List<Subscriber> subscribers = getSubsribers();
+
+        DateFormat dateFormatter = new SimpleDateFormat("yyyy-MM-dd_HH-mm-ss");
+        String currentDateTime = dateFormatter.format(new Date());
+        String filename = "subscribers_" + currentDateTime + ".csv";
+
+        String[] headers = {"Subscriber ID"
+                , "Name"
+                , "E-Mail"
+                , "Phone"
+                , "Address"
+                , "Property ID"
+                , "Property Name"
+                , "Property Title"
+                , "Date"
+        };
+
+        FileWriter out = new FileWriter(filename);
+        try (CSVPrinter printer = new CSVPrinter(out, CSVFormat.DEFAULT
+                .withHeader(headers))) {
+            for (Subscriber subscriber : subscribers) {
+                printer.printRecord(subscriber);
+            }
+        }
+
+        return FileSystems.getDefault().getPath(filename);
     }
 
 }
